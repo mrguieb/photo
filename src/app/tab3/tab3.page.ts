51ed8083033fe6df739e5ef6940e6f1937a7a9fd
emgie <emgieguieb110801@gmail.coom>
2025-04-19 23:17:58 +0800

    added my location

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import { IonicModule } from '@ionic/angular'
+
+
+declare var google: { maps: { Map: new (arg0: HTMLElement, arg1: { center: { lat: number; lng: number; }; zoom: number; }) => any; Marker: new (arg0: { position: { lat: number; lng: number; }; map: any; title: string; }) => any; }; }
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-  standalone: false,
+selector: 'app-tab3',
+templateUrl: 'tab3.page.html',
+styleUrls: ['tab3.page.scss'],
+standalone: true,
+imports: [IonicModule],
 })
-export class Tab3Page {
+export class Tab3Page implements AfterViewInit {
 
-  constructor() {}
+async ngAfterViewInit() {
+const coordinates = await Geolocation.getCurrentPosition();
+const position = {
 
+lat: coordinates.coords.latitude,
+lng: coordinates.coords.longitude
+};
+
+const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+center: position,
+zoom: 15
+});
+
+new google.maps.Marker({
+position,
+map,
+title: "You are here!"
+});
+}
 }
